5bc541751798120e9e28a9c119a1fe5f41190d45
Aluno do Inatel <aluno@local.inatel.br>
2017-11-04 16:03:54 -0200

    Aula 1 de dm110.

---
+++ dm110-impl/src/main/java/br/inatel/dm110/hello/impl/HelloServiceImpl.java
@@ -0,0 +1,23 @@
+package br.inatel.dm110.hello.impl;
+
+import br.inatel.dm110.hello.api.HelloService;
+import br.inatel.dm110.hello.api.Result;
+
+public class HelloServiceImpl implements HelloService{
+
+	@Override
+	public String sayHello(String name) {
+		return "<h1>Hello " + name + "!</h1>";
+	}
+
+	@Override
+	public Result message(String first, String last) {
+		Result result = new Result();
+		result.setFirstName(first);
+		result.setLastName(last);
+		result.setMessage(String.format("Hello %s %s!", first, last));
+				
+		return result;
+	}
+
+}
